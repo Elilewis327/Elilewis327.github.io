74ff5fc02fcfa2179a01bc9063442789e973c46d
eli <etl3@calvin.edu>
2024-10-22 15:29:35 -0400

    init

--- Sandy/index.js
+++
@@ -1,8 +0,0 @@
-function no_scroll(a){
-    element = document.getElementById("main");
-    if (a){
-        element.classList.add("noscroll");
-    } else {
-        element.classList.remove("noscroll");
-    }
-}
